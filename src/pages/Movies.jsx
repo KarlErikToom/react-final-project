ce422918a0d6505b07bc421e36288de4ab98387f
Karl <toom.karl42@gmail.com>
2023-07-31 14:54:34 +0300

    fixed issue with loading state not showing

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -20,7 +20,7 @@ function Movies() {
   let navigate = useNavigate();
 
   function onSearch() {
-    setLoading(true)
+    setLoading(true);
     fetchMovies(searchTitle);
   }
   async function fetchMovies(searchTitle) {
@@ -36,8 +36,8 @@ function Movies() {
         Object.assign(movie, response.data);
       }
       setMovies(data);
-    } else{
-      setMovies([])
+    } else {
+      setMovies([]);
     }
     setLoading(false);
   }
@@ -55,7 +55,9 @@ function Movies() {
       <header className="input__head">
         <div className="input__wrapper">
           <div className="input__header">
-            <Link to={"/"} className="logo">MovieWiki</Link>
+            <Link to={"/"} className="logo">
+              MovieWiki
+            </Link>
           </div>
           <div className="input">
             <input
@@ -73,88 +75,80 @@ function Movies() {
       </header>
       <div className="container">
         <div className="row">
-          <div>
-            {movies.Search === undefined ? (
-              <div className="no-results">
-                <div className="no-results__text">
-                  <h1>We'll wait while you choose what to search for </h1>
-                  <h2>
-                    We got info on your favorite Movies, Tv-shows, books and
-                    even games
-                  </h2>
-                  <h3>Whatever your looking for, we got it</h3>
+        {loading ? (
+            <div className="movies">
+              {new Array(6).fill(0).map((element, index) => (
+                <div className="movie" key={element || index}>
+                  <div className="movie__wrapper">
+                    <figure className="movie__img--figure">
+                      <a href="">
+                        <img
+                          className="movie__img movie__img--skeleton"
+                          alt=""
+                        />
+                      </a>
+                    </figure>
+                    <div className="movie__info">
+                      <p className="movie__title--skeleton"></p>
+                      <p className="movie__year--skeleton"></p>
+                    </div>
+                  </div>
                 </div>
-                <img className="no-results__img" src={wait} alt="" />
+              ))}
+            </div>
+          ) : movies.Search=== undefined ? (
+            <div className="no-results">
+              <div className="no-results__text">
+                <h1>We'll wait while you choose what to search for</h1>
+                <h2>We got info on your favorite Movies, TV shows, books, and even games</h2>
+                <h3>Whatever you're looking for, we got it</h3>
               </div>
-            ) : loading ? (
-              <div className="movies">
-                {new Array(6).fill(0).map((element, index) => (
-                  <div className="movie" key={element || index}>
-                    <div className="movie__wrapper">
-                      <figure className="movie__img--figure">
-                        <a href="">
-                          <img
-                            className="movie__img movie__img--skeleton"
-                            alt=""
-                          />
-                        </a>
-                      </figure>
-                      <div className="movie__info">
-                        <p className="movie__title--skeleton"></p>
-                        <p className="movie__year--skeleton"></p>
-                      </div>
+              <img className="no-results__img" src={wait} alt="" />
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.Search?.slice(0, 6).map((movie) => (
+                <div className="movie" key={movie.imdbID}>
+                  <div className="movie__wrapper">
+                    <figure className="movie__img--figure">
+                      <a href="">
+                        <img
+                          className="movie__img"
+                          onClick={() => navigate(`${movie.imdbID}/`)}
+                          src={
+                            movie.Poster !== "N/A" ? movie.Poster : noImage
+                          }
+                          alt=""
+                        />
+                      </a>
+                    </figure>
+                    <div className="movie__info">
+                      <p className="movie__title">{movie.Title}</p>
+                      <p className="movie__rating">
+                        <b>IMDB rating</b>
+                        <FontAwesomeIcon className="star" icon={faStar} />{" "}
+                        <span className="rating">{movie.imdbRating}</span>
+                      </p>
+                      <p className="movie__summary">
+                        {movie.Plot.substring(0, 90)}...
+                      </p>
+                      <p className="movie__type">
+                        {" "}
+                        <b className="bold">Type</b> {movie.Type}
+                      </p>
+                      <p className="movie__genre">
+                        {" "}
+                        <b>Genre</b> {movie.Genre}
+                      </p>
+                    </div>
+                    <div className="movie__votes">
+                      <FontAwesomeIcon icon={faThumbsUp} /> {movie.imdbVotes}
                     </div>
                   </div>
-                ))}
-              </div>
-            ) : (
-              <div className="movies">
-                {movies.Search?.slice(0, 6).map((movie) => (
-                  <div className="movie" key={movie.imdbID}>
-                    <div className="movie__wrapper">
-                      <figure className="movie__img--figure">
-                        <a href="">
-                          <img
-                            className="movie__img"
-                            onClick={() => navigate(`${movie.imdbID}/`)}
-                            src={
-                              movie.Poster !== "N/A" ? movie.Poster : noImage
-                            }
-                            alt=""
-                          />
-                        </a>
-                      </figure>
-                      <div className="movie__info">
-                        <p className="movie__title">{movie.Title}</p>
-                        <p className="movie__rating">
-                          <b>IMDB rating</b>
-                          <FontAwesomeIcon
-                            className="star"
-                            icon={faStar}
-                          />{" "}
-                          <span className="rating">{movie.imdbRating}</span>
-                        </p>
-                        <p className="movie__summary">
-                          {movie.Plot.substring(0, 90)}...
-                        </p>
-                        <p className="movie__type">
-                          {" "}
-                          <b className="bold">Type</b> {movie.Type}
-                        </p>
-                        <p className="movie__genre">
-                          {" "}
-                          <b>Genre</b> {movie.Genre}
-                        </p>
-                      </div>
-                      <div className="movie__votes">
-                        <FontAwesomeIcon icon={faThumbsUp} /> {movie.imdbVotes}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </section>
